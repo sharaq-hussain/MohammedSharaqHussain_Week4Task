89036e6b55c7062e6be55c9e53fbf8f0c77ef7c3
sharaq-hussain <mohdsharaq5@gmail.com>
2025-11-01 14:55:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ async function fetchQuote() {
   authorText.style.opacity = 0;
 
   try {
-    const response = await fetch('http://localhost:3000/quote');
+    const response = await fetch('https://zenquotes.io/api/random');
     const data = await response.json();
 
     quoteText.innerText = `"${data.content}"`;
